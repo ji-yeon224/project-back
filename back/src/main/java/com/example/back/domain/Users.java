fe99bb3d4fd1ff34260ee18d1ca779de5883e3e1
Kim-jiyeon224 <shjh0107@gmail.com>
2022-05-30 16:24:55 +0900

    Initial commit

---
+++ back/src/main/java/com/example/back/domain/Users.java
@@ -0,0 +1,26 @@
+package com.example.back.domain;
+
+import lombok.Data;
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    String username;
+    String password;
+    String firstName;
+    String lastName;
+    int age;
+    int salary;
+
+}
---
+++ back/src/main/java/com/example/back/repository/UserRepository.java
@@ -0,0 +1,2 @@
+package com.example.back.repository;public class UserRepository {
+}
